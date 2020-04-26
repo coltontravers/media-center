3effd01674909d08747ab092602f98ae7f000c3b
Colton Travers <contactcolt@gmail.com>
2020-04-25 21:14:45 -0400

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+require("colors");
+const path = require("path");
+const PrettierPlugin = require("prettier-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader", "eslint-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ["*", ".js", ".jsx"],
+    },
+    plugins: [new PrettierPlugin()],
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, "public"),
+        onListening: (server) => {
+            const { port, address } = server.listeningApp.address();
+
+            // eslint-disable-next-line no-console
+            console.log(
+                "Front-end started at:".green,
+                `http://${address}:${port}`.underline
+            );
+        },
+        stats: "minimal",
+        noInfo: true,
+    },
+};

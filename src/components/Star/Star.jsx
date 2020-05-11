b03b6430de7e410c47e1d02b48614d4600192759
Colton Travers <contactcolt@gmail.com>
2020-05-10 22:20:14 -0400

    Flesh out star component into something usable.

--- src/components/Star/Star.jsx
+++ src/components/Star/Star.jsx
@@ -1,9 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { StyledStar } from "./Star.styled";
 
 const Star = ({ filled }) => {
-    return <StyledStar filled={filled} />;
+    return (
+        <>
+            <StyledStar filled={filled}>
+                <FontAwesomeIcon icon={faStar} size="4x" />
+            </StyledStar>
+
+            {/* Ugly, but this is for applying a linear gradient as a background to the svg */}
+            <svg width="0" height="0">
+                <linearGradient
+                    spreadMethod="pad"
+                    id="lgrad"
+                    x1="0%"
+                    y1="0%"
+                    x2="100%"
+                    y2="0%"
+                >
+                    <stop
+                        offset={filled}
+                        style={{
+                            stopColor: "rgb(254, 229, 0)",
+                            stopOpacity: 1
+                        }}
+                    />
+                    <stop
+                        offset={filled}
+                        style={{
+                            stopColor: "silver",
+                            stopOpacity: 0.2
+                        }}
+                    />
+                </linearGradient>
+            </svg>
+        </>
+    );
 };
 
 Star.propTypes = {
--- src/components/Star/Star.styled.js
+++ src/components/Star/Star.styled.js
@@ -1,14 +1,12 @@
 import styled from "styled-components";
 
 export const StyledStar = styled.div`
-postion: relative;
+    svg {
+        stroke: #9e9e9e;
+        stroke-width: 5px;
 
-::after {
-    content: "",
-    position: absolute;
-    top: 0;
-    left: 0;
-    background: yellow;
-    width: ${({ filled }) => filled};
-}
+        * {
+            fill: url(#lgrad);
+        }
+    }
 `;
